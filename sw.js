a352224ae9cc3021ee729c19e0b2d23cb7102b30
monsivar <32893134+monsivar@users.noreply.github.com>
2026-09-20 21:36:54 +0200

    Use Arbeiderpartiet rose as app logo

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'einnsyn-buskerud-shell-v1.0.11';
+const CACHE_NAME = 'einnsyn-buskerud-shell-v1.0.12';
 const APP_SHELL = [
   './',
   './index.html',
   './styles.css',
-  './app.js?v=1.0.11',
+  './app.js?v=1.0.12',
   './arbeiderpartiet-rose.png',
   './manifest.json',
   './icon-180.png',
